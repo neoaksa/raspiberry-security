d60ae84af4b8764fa6ca2636365bf1dd448dfc20
Jie Tao <jtao@aclaratech.com>
2019-10-16 17:01:16 -0400

    add DNN reference

---
+++ object_detect.py
@@ -0,0 +1,11 @@
+import cv2
+
+def classify_frame(net, inputframe, output):
+    # initialize the list of class labels MobileNet SSD was trained to
+    # detect, then generate a set of bounding box colors for each class
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+        "sofa", "train", "tvmonitor"]
+
+
